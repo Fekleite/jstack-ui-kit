18357fe40ed55687bc0e0d365bc5cd787508ada7
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-05-12 20:34:02 -0300

    feat: setup tree shaking to build only used components on consumer

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: [
+    'src/**/*.{ts,tsx}',
+    'src/styles/index.css',
+    '!src/**/*.stories.{ts,tsx}',
+  ],
   format: ['cjs', 'esm'],
   outDir: 'dist',
   tsconfig: 'tsconfig.build.json',
